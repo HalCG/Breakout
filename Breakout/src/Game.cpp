aad5c2e27ef764fa3740d0fe3babd0ca6ab9e1d5
lichengfeng <tolcf@qq.com>
2024-12-02 17:44:15 +0800

    方形、圆形碰撞检测

--- Breakout/src/Game.cpp
+++ Breakout/src/Game.cpp
@@ -115,11 +115,71 @@ void Game::Render()
         Renderer->DrawSprite(backgroundTexture,
             glm::vec2(0.0f, 0.0f), glm::vec2(this->Width, this->Height), 0.0f
         );
-        // draw level
+        // draw levels
         this->Levels[this->Level].Draw(*Renderer);
     }
 
+    //draw player
     Player->Draw(*Renderer);
 
+    //draw ball
     Ball->Draw(*Renderer);
+
+    //碰撞检测
+    this->DoCollisions();
 }
+
+//方形碰撞检测
+bool CheckCollision_square(GameObject& one, GameObject& two) // AABB - AABB collision
+{
+    // collision x-axis?
+    bool collisionX = one.Position.x + one.Size.x >= two.Position.x &&
+        two.Position.x + two.Size.x >= one.Position.x;
+    // collision y-axis?
+    bool collisionY = one.Position.y + one.Size.y >= two.Position.y &&
+        two.Position.y + two.Size.y >= one.Position.y;
+    // collision only if on both axes
+    return collisionX && collisionY;
+}
+
+//圆形碰撞检测
+//float clamp(float value, float min, float max) {
+//    return std::max(min, std::min(max, value));
+//}
+
+bool CheckCollision_round(BallObject& one, GameObject& two) // AABB - Circle collision
+{
+    // 获取球圆心
+    glm::vec2 center(one.Position + one.Radius);
+    // 计算GameObject 中心
+    glm::vec2 aabb_half_extents(two.Size.x / 2.0f, two.Size.y / 2.0f);
+    glm::vec2 aabb_center(
+        two.Position.x + aabb_half_extents.x,
+        two.Position.y + aabb_half_extents.y
+    );
+    // 获取两向量中心差
+    glm::vec2 difference = center - aabb_center;//可以理解为两个中心的连线
+    //找到方框中心与经过边框的向量
+    glm::vec2 clamped = glm::clamp(difference, -aabb_half_extents, aabb_half_extents);//约定范围-aabb_half_extents, aabb_half_extents
+    // 获取邻近点的位置
+    glm::vec2 closest = aabb_center + clamped;
+    // 获取点位置与圆球中心距离
+    difference = closest - center;
+    return glm::length(difference) < one.Radius;
+}
+
+void Game::DoCollisions()
+{
+    for (GameObject& box : this->Levels[this->Level].Bricks)
+    {
+        if (!box.Destroyed)
+        {
+            if (CheckCollision_round(*Ball, box))
+            {
+                if (!box.IsSolid)
+                    box.Destroyed = true;
+            }
+        }
+    }
+}
+
